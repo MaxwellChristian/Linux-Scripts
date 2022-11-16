9bd5caf7b6810c2f7324b3f68cb97287a558b1d6
Maxwell Christian <112983367+MaxwellChristian@users.noreply.github.com>
2022-11-16 12:24:51 -0600

    Maxwell Christian

--- src/main/scripts/ordering.sh
+++ src/main/scripts/ordering.sh
@@ -24,7 +24,26 @@
 #   hello
 #   welcome
 
-sort < file.txt
+#sort < file.txt
+
+##################################################
+# sort the entries from the log in order of the resource name
+#sort -k 7 < data/log.log
+# the -k option specifies the key field to be used for sorting
+# the default order of sorting is ascending
+
+##################################################
+# sort the entries from the log in descending order of the resource name
+#sort -r -k 7 < data/log.log
+# the -r option specifies the reverse order of sorting
+
+##################################################
+# sort the entries from the log in descending order of the fetched bytes
+sort -r -n -k 10 < data/log.log
+
+##################################################
+# display the most recent 50 largest files accessed
+sort -r -n -k 10 < data/log.log | head -n50
 
 ##################################################
 # halt to capture input from user
